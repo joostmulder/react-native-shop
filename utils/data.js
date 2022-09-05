ed70e04223abf64721976b7ea6e172cc519854d3
Marcos Opolis <afteryou725@gmail.com>
2022-09-05 20:28:55 +0900

    change product names

---
+++ utils/data.js
@@ -0,0 +1,107 @@
+export const CATEGORIES_DATA = [
+  {
+    name: 'Womens',
+    url: 'http://www1.macys.com/shop/womens-clothing?id=118&edge=hybrid',
+    child: [
+      {
+        id: 10066,
+        name: 'Sale & Clearance',
+        imgURL: require('../app/images/womens/sale.png')
+      },
+      {
+        id: 22006,
+        name: "Women's Clothing",
+        imgURL: require('../app/images/womens/clothing.png')
+      },
+      {
+        id: 225,
+        name: 'Bras, Panties & Lingerie',
+        imgURL: require('../app/images/womens/lingerie.png')
+      },
+      {
+        id: 56233,
+        name: "Women's Shoes",
+        imgURL: require('../app/images/womens/shoes.png')
+      },
+      {
+        id: 26846,
+        name: 'Handbags & Accessories',
+        imgURL: require('../app/images/womens/handbags.png')
+      },
+      {
+        id: 669,
+        name: 'Beauty',
+        imgURL: require('../app/images/womens/beauty.png')
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: 'Mens',
+    url: 'http://www1.macys.com/shop/mens-clothing?id=1&edge=hybrid',
+    child: [
+      {
+        id: 9559,
+        name: 'Sale & Clearance',
+        imgURL: require('../app/images/mens/sale.png')
+      },
+      {
+        id: 22050,
+        name: "Men's Clothing",
+        imgURL: require('../app/images/mens/clothing.png')
+      },
+      {
+        id: 70655,
+        name: "Men's Shoes",
+        imgURL: require('../app/images/mens/shoes.png')
+      },
+      {
+        id: 70504,
+        name: 'Activewear',
+        imgURL: require('../app/images/mens/activewear.png')
+      },
+      {
+        id: 22051,
+        name: 'Bags & Accessories',
+        imgURL: require('../app/images/mens/accessories.png')
+      },
+      {
+        id: 186572,
+        name: 'Cologne & Grooming',
+        imgURL: require('../app/images/mens/cologne.png')
+      },
+    ]
+  },
+  {
+    id: 5991,
+    name: 'Kids',
+    url: 'http://www1.macys.com/shop/kids-clothes?id=5991&edge=hybrid',
+    child: [
+      {
+        id: 6086,
+        name: 'Sale & Clearance',
+        imgURL: require('../app/images/kids/sale.png')
+      },
+      {
+        id: 48691,
+        name: 'Baby',
+        imgURL: require('../app/images/kids/baby.png')
+      },
+      {
+        id: 63007,
+        name: "Kids' Clothing",
+        imgURL: require('../app/images/mens/clothing.png')
+      },
+      {
+        id: 188667,
+        name: "Kids' Shoes",
+        imgURL: require('../app/images/kids/shoes.png')
+      },
+      {
+        id: 188668,
+        name: 'Accessories',
+        imgURL: require('../app/images/kids/accessories.png')
+      },
+    ]
+  }
+];
